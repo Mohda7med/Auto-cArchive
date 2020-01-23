3b61e0f4c6118d7da4bb7c3d139b8d0b2b790c23
Mohammed Ahmed <mohammed@Mohammeds-MacBook-Air.local>
2020-01-23 10:24:29 -0800

    adding new code file

---
+++ test2.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import pandas as pd
+import boto3
+
+bucket_name = "nyc-taxi-rideshare"
+# file_name = "trip_data/fhv_tripdata_2015-01.csv"
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(bucket_name)
+
+# Iterates through all the objects, doing the pagination for you. Each obj
+# is an ObjectSummary, so it doesn't contain the body. You'll need to call
+# get to get the whole body.
+
+for obj in bucket.objects.all():
+  if  obj.size > 0:
+    key = obj.key
+    if 'green_tripdata_2019-02' in key:
+      body = obj.get()['Body']
+
+      data = pd.read_csv(body)
+      print (data.head())
